194fc7797b61e99bcf83261a19c97c8eb7eead8e
Steven Nemetz <snemetz@book.com>
2014-07-11 01:06:01 +0000

    Change order. See if effects codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'coveralls'
-require 'codeclimate-test-reporter'
 
-CodeClimate::TestReporter.start
 Coveralls.wear!
 
 RSpec.configure do |c|
